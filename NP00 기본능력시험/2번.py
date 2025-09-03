d7d2e09ab50d137181a813a5fa8e9e3597421ca7
MoonHyoWon <penguin001127@gmail.com>
2025-09-03 13:57:02 +0900

    First Commit

---
+++ NP00 기본능력시험/2번.py
@@ -0,0 +1,25 @@
+# 2. 다음 요구사항에 맞는 변수 선언하기
+#   1) 정수 5를 저장하는 변수, 
+#   2) 실수 2.14를 저장하는 변수, 
+#   3) “Hello”를 저장하는 변수, 숫자의 목록 1,2,3,4를 저장하는 변수, 
+#   4) 문자열 목록 “Apple”, “Banana”, “Cherry”, “Durian”을 저장하는 변수, 
+#   5) 원주율 3.141592 상수를 저장하는 매크로 변수 혹은 상수형 변수 선언, 
+#   6) 자신의 이름, 나이, 전화번호로 초기화된 구조체 변수 선언
+# 학번 : 202311398
+# 작성자 : 문효원
+
+p_num = 5
+f_num = 2.14
+char = "Hello"
+index = [1, 2, 3, 4]
+char_index = ["Apple", "Banana", "Cherry", "Durian"]
+PI = 3.141592
+people = ["문효원", 22, "01046212439"]
+
+print("1번. p_num = ", p_num)
+print("2번. f_num = ", f_num)
+print("3-1번. char = ", char)
+print("3-2번. index = ", index)
+print("4번. char_index = ", char_index)
+print("5번. PI = ", PI)
+print("6번. people = ", people)
